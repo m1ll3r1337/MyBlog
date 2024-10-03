647d0326ca1c4d3980798404f2839ac4975fa13b
m1ll3r1337 <mah235715@gmail.com>
2024-10-03 11:59:44 +0300

    implemented session service user

--- models/session.go
+++ models/session.go
@@ -66,7 +66,19 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`SELECT user_id FROM Sessions WHERE token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	row = ss.DB.QueryRow(`SELECT email, password FROM Users WHERE id = $1;`, user.ID)
+	err = row.Scan(&user.Email, &user.Password)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	return &user, err
 }
 
 func (ss *SessionService) hash(token string) string {
